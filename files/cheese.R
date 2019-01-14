b4a0764d80af41edea7ba44ef1d9a2a572e94b4f
Logan Carter <logan.a.carter@gmail.com>
2019-01-14 16:20:20 -0700

    commit

--- files/cheese.R
+++ files/cheese.R
@@ -6,13 +6,20 @@
 # observation.  Name your merged data.frame "cheese".  The dimension of this
 # data.frame should be 20 rows and 7 columns.
 
+cheese1 <- read.table(file="~/Desktop/STAT223/hw1-starter/files/cheese1.csv", header = TRUE, sep = ",")
+cheese2 <- read.table(file="~/Desktop/STAT223/hw1-starter/files/cheese2.csv", header=TRUE, sep=",")
+cheese <- merge(x = cheese1,y = cheese2, by.x = "Year", by.y = "Time")
+
+dim(cheese)
 
 
 # Which kind of cheese has the highest average consumption per capita over all
 # years?
+avgs <- apply(cheese[,names(cheese) != "Year"], 2, mean)
+names(which.max(avgs))
 
 
 
 # Which year saw the largest total consumption per capita of all cheeses?
-
-
+totals <- apply(cheese[,names(cheese) != "Year"], 1, sum)
+cheese$Year[which.max(totals)]
